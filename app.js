4dbc7ed39c18880b8408a6fd0779685b91678f5e
Ashutosh Verma <vtanya197@gmail.com>
2021-10-08 00:42:34 -0400

    Try

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post('/charge', (req,res) => {
     .then(charge => res.render('success'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`);
