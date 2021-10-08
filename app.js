cfb2f4f0ae4d70d3006699c3665af4a4defb60c0
Ashutosh Verma <vtanya197@gmail.com>
2021-10-08 00:50:50 -0400

    Try

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.engine('handlebars', expHbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //body parser middle ware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 //set static folder
 app.use(express.static(`${__dirname}/public`));
@@ -41,7 +41,7 @@ app.post('/charge', (req,res) => {
     .then(charge => res.render('success'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || '5000';
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`);
